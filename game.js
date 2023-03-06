6ddd0a099fab7fd3996b34e4a51bd1d96ab04db0
Johnathan <johnathanferreira02@gmail.com>
2023-03-06 12:41:06 -0300

    Add turn logic

--- game.js
+++ game.js
@@ -2,16 +2,46 @@
 function getComputerChoice(){
     let min = 1;
     let max = 3;
+    let computer;
     
     let random = Math.floor(Math.random() * (max - min + 1) + min);
 
     if(random === 1){
-        console.log("Rock");
+        computer = "rock";
     }else if(random === 2){
-        console.log("Paper");
+        computer = "paper";
     }else{
-        console.log("Scissors");
+        computer = "scissors";
     }
+    return computer;
 }
 
-getComputerChoice();
+function turn(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        console.log("tie");
+    }
+    if(playerSelection === "rock") {
+        if(computerSelection === "scissors"){
+            console.log("You won! Rock beats Scissors");
+        }else if(computerSelection === "paper"){
+            console.log("You lose! Paper beats rock");
+        }
+    } else if(playerSelection === "paper"){
+        if(computerSelection === "rock"){
+            console.log("You won! Paper beats rock");
+        }else if(computerSelection === "scissors"){
+            console.log("You lose! Scissors beats paper");
+        }
+    } else if(playerSelection === "scissors"){
+        if(computerSelection === "rock"){
+            console.log("You lose! Rock beats Scissors");
+        }else if(computerSelection === "paper"){
+            console.log("You won! Scissors beats paper");
+        }
+    }
+}
+let playerSelection = prompt("Rock - Paper - Scissors");
+let player1 = playerSelection.toLowerCase();
+let computerSelection = getComputerChoice();
+
+turn(player1, computerSelection);
